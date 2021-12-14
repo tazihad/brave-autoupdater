a2dfdefda2761da1e59e244279a55da9842d84bb
Tarek <tazihad@gmail.com>
2021-12-15 02:27:18 +0600

    clean up script

--- brave-auto-updater.sh
+++ brave-auto-updater.sh
@@ -4,18 +4,20 @@ RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
+INSTALLATION_DIRECTORY="/home/$USER/.opt/brave"
+TEMP_DIRECTORY="/home/$USER/Downloads/000bravetemp"
 
 update_browser() {
 
     echo "--> Removing previous temp folder"
-    rm -rf ~/Downloads/000bravetemp
+    rm -rf $TEMP_DIRECTORY
 
     echo "--> Creating temp folder"
-    mkdir -p ~/Downloads/000bravetemp
+    mkdir -p $TEMP_DIRECTORY
 
-    echo "--> Brave browser will be installed at ~/.opt/brave/"
-    mkdir -p ~/.opt/brave
-    cd ~/Downloads/000bravetemp
+    echo "--> Brave browser will be installed at $INSTALLATION_DIRECTORY/"
+    mkdir -p $INSTALLATION_DIRECTORY
+    cd $TEMP_DIRECTORY
 
     echo "--> Searching and downloading latest stable Brave Browser from https://github.com/brave/brave-browser/releases"
     wget -O brave.deb -q --show-progress https://github.com/brave/brave-browser/releases/download/v"$(curl -s https://brave-browser-downloads.s3.brave.com/latest/release.version)"/brave-browser_"$(curl -s https://brave-browser-downloads.s3.brave.com/latest/release.version)"_amd64.deb
@@ -30,10 +32,10 @@ update_browser() {
     cd opt/brave.com
 
     echo "--> Moving Downloded files to installed directory"
-    cp -TR brave ~/.opt/brave
+    cp -TR brave $INSTALLATION_DIRECTORY/
 
     echo "--> Removing temp folder"
-    rm -rf ~/Downloads/000bravetemp
+    rm -rf $TEMP_DIRECTORY
 
     echo "---: Completed :---"
 
@@ -42,10 +44,10 @@ update_browser() {
 }
 
 
-FILE=/home/$USER/.opt/brave/brave-browser
+FILE=$INSTALLATION_DIRECTORY/brave-browser
 if [ -f "$FILE" ]; then
 
-    cd ~/.opt/brave
+    cd $INSTALLATION_DIRECTORY
     echo -e "===> Installed version: "${RED}"$(grep -Po "(?<=tag/v)([0-9]|\.)*(?=\s|$)" changelog)"${ENDCOLOR}""
     echo -e "===> Latest version: "${RED}"$(curl -s https://brave-browser-downloads.s3.brave.com/latest/release.version)"${ENDCOLOR}""
 
@@ -58,7 +60,7 @@ if [ -f "$FILE" ]; then
     done
 
 else
-    echo "-->Brave not installed or Not found at ~/.opt/brave/"
+    echo "-->Brave not installed or Not found at $INSTALLATION_DIRECTORY/"
 
     echo "-->Do you want to install Brave Browser?"
     select yn in "Yes" "No"; do
